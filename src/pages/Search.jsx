bb0eb62e40abaefda2a46109a919bb8831db30f3
leeseayoung <tnfusrltk12@naver.com>
2024-10-18 02:42:59 +0900

    feat: 무한스크롤 성공!

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,28 +1,56 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 import { customAxios } from "../api/axios";
 import { useSearchParams } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
+import { useEffect } from "react";
 
-const fetchSearch = async (search) => {
-  const response = await customAxios.get(`/search/photos?query=${search}`);
+const fetchSearch = async (search, pageParam) => {
+  const response = await customAxios.get(
+    `/search/photos?query=${search}&page=${pageParam}`
+  );
   return response.data;
 };
 
 const Search = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const query = searchParams.get("q");
 
-  const { data } = useQuery({
-    queryKey: [`search`, query],
-    queryFn: () => fetchSearch(query),
-  });
-
   const [ref, inView] = useInView();
-  console.log(inView);
+
+  const { data, fetchNextPage, isFetchingNextPage } = useInfiniteQuery({
+    queryKey: ["infiniteSearch", query],
+    queryFn: ({ pageParam = 1 }) => {
+      return fetchSearch(query, pageParam);
+    },
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) => {
+      const lastPageNumber = lastPage.total_pages;
+      const currentPageNumber = pages.length + 1;
+
+      if (lastPageNumber > currentPageNumber) {
+        return pages.length + 1;
+      }
+      return undefined;
+    },
+
+    select: (data) => {
+      return {
+        pages: data?.pages.flatMap((page) => page.results),
+        pageParams: data.pageParams,
+      };
+    },
+  });
+  console.log(data);
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView]);
+
   return (
     <div>
-      {data?.results?.map((item) => {
+      {data?.pages?.map((item) => {
         return <img key={item.urls.regular} src={item.urls.regular} />;
       })}
       <Div ref={ref}></Div>
