61b48002971df314c4af33fc6a4ac659fdf4124f
leeseayoung <tnfusrltk12@naver.com>
2024-10-19 00:45:30 +0900

    feat: useInfiniteQuery를 커스텀 훅 분리

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,39 +1,15 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
 import { useInView } from "react-intersection-observer";
 import styled from "styled-components";
 import { useEffect } from "react";
-import { fetchSearch } from "../api/search";
+import useInfiniteSearchPhotos from "../hooks/useInfiniteSearchPhotos";
 
 const Search = () => {
   const [searchParams] = useSearchParams();
   const query = searchParams.get("q");
-
   const [ref, inView] = useInView();
 
-  const { data, fetchNextPage } = useInfiniteQuery({
-    queryKey: ["infiniteSearch", query],
-    queryFn: ({ pageParam = 1 }) => {
-      return fetchSearch(query, pageParam);
-    },
-    initialPageParam: 1,
-    getNextPageParam: (lastPage, pages) => {
-      const lastPageNumber = lastPage.total_pages;
-      const currentPageNumber = pages.length + 1;
-
-      if (lastPageNumber > currentPageNumber) {
-        return pages.length + 1;
-      }
-      return undefined;
-    },
-
-    select: (data) => {
-      return {
-        pages: data?.pages.flatMap((page) => page.results),
-        pageParams: data.pageParams,
-      };
-    },
-  });
+  const { data, fetchNextPage } = useInfiniteSearchPhotos(query);
 
   useEffect(() => {
     if (inView) {
