8d38e2206f0d7b0005df47dc216acc350819ceb9
leeseayoung <tnfusrltk12@naver.com>
2024-11-19 18:09:06 +0900

    feat: 스크롤시 헤더 고정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,35 @@
+import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import MenuHeader from "./MenuHeader";
 
 const Layout = () => {
   return (
-    <div>
-      <Header />
-      <MenuHeader />
-      <Outlet />
-    </div>
+    <>
+      <FixedHeader>
+        <Header />
+        <MenuHeader />
+      </FixedHeader>
+      <MainContent>
+        <Outlet />
+      </MainContent>
+    </>
   );
 };
+
+const FixedHeader = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background: white;
+  z-index: 1000;
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const MainContent = styled.main`
+  margin-top: 116px;
+  /* padding: 20px; */
+`;
 
 export default Layout;
