713a3e4649b511efaebd9f88fe2e4b2278702c53
leeseayoung <tnfusrltk12@naver.com>
2024-11-20 14:38:59 +0900

    revert: 헤더 고정 레이아웃 구조 원복

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,35 +1,15 @@
-import styled from "styled-components";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import MenuHeader from "./MenuHeader";
 
 const Layout = () => {
   return (
-    <>
-      <FixedHeader>
-        <Header />
-        <MenuHeader />
-      </FixedHeader>
-      <MainContent>
-        <Outlet />
-      </MainContent>
-    </>
+    <div>
+      <Header />
+      <MenuHeader />
+      <Outlet />
+    </div>
   );
 };
-
-const FixedHeader = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  background: white;
-  z-index: 1000;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-`;
-
-const MainContent = styled.main`
-  margin-top: 116px;
-  /* padding: 20px; */
-`;
 
 export default Layout;
